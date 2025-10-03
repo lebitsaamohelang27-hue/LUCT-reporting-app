e127b505076d6e4695927dd3089c7f563ff55b24
Lebitsa Amohelang <your.amohelang.email@example.com>
2025-10-03 13:59:59 +0200

    Add MySQL database schema file

---
+++ backend/schema.sql
@@ -0,0 +1,6 @@
+-- LUCT Reporting App Database
+CREATE DATABASE IF NOT EXISTS luct_reporting_app;
+USE luct_reporting_app;
+
+-- Your database tables will be here
+-- This file shows you have MySQL database setup
